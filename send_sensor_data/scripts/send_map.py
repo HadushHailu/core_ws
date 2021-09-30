1dd53f6c4ce6c0f1c8d536f8b4c85220406a8b7e
Hadush <had.hailu@gmail.com>
2021-09-30 20:30:07 +0900

    msg_ntk added

--- send_sensor_data/scripts/send_map.py
+++ send_sensor_data/scripts/send_map.py
@@ -12,26 +12,32 @@ LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(level=logging.INFO)
 
 ## ws_client and msg_ntk path 
-sys.path.insert(0, os.path.abspath("../comm/msg_ntk"))
-LOGGER.info("current path: %s", os.path.abspath("../comm/msg_ntk"))
+sys.path.insert(0, os.path.abspath("../uni_auto_comm/msg_ntk"))
+LOGGER.info("current path: %s", os.path.abspath("../uni_auto_comm/msg_ntk"))
 
 from msg_ntk import Map2DDataPUB
 
 
+##
+class SendMap():
+    def __init__(self):
+        # init
+        rospy.init_node('send_map', anonymous=True) 
+        rospy.Subscriber("map", OccupancyGrid, self.map_callback)
 
-def map_callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data)
+        # init msg_ntk
+        self.map_2d_data_pub = Map2DDataPUB()
 
-    # msg_ntk
-    
-def send_map():
+    def map_callback(self,msg):
+        rospy.loginfo(rospy.get_caller_id() + "Map data recieved")
 
-    rospy.init_node('send_map', anonymous=True)
+        # msg_ntk
+        self.map_2d_data_pub.pub("%s" % msg)
 
-    rospy.Subscriber("map", OccupancyGrid, map_callback)
-
-    # thread
-    rospy.spin()
+    def start_it(self):
+        # thread
+        rospy.spin()
 
 if __name__ == '__main__':
-    send_map()
+    app = SendMap()
+    app.start_it()
